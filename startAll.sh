b7d7bc796ea153d2bd41168dfa2f304a5114d4be
des <37875869+greedyfox7@users.noreply.github.com>
2021-03-08 04:53:51 -0800

    Update and rename var11.sh to startAll.sh

---
+++ startAll.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "\tЗапуск 1"
+sh ./start.sh
+sleep 2
+read intput
+echo "\n\n\n\tЗапуск 2"
+sleep 2
+sh ./startOut.sh
+sleep 2
+read intput
+echo "\n\n\n\tЗапуск 3"
+sh ./startAsm.sh
+sleep 2
+/bin/bash

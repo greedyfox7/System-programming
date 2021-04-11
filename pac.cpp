e46f946af2e6a3b88c6eb9da520b656978fd3f29
des <37875869+greedyfox7@users.noreply.github.com>
2021-04-11 01:17:58 -0700

    Create pac.cpp

---
+++ pac.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <map>
+#include <fstream>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <iterator>
+
+using namespace std;
+
+vector<string> trigrams(string w) {
+    vector<string> grams;
+
+    for (int i = 0; i < w.length() - 2; i++)
+    {
+        grams.push_back(w.substr(i, 3));
+    }
+
+    return grams;
+}
+
+int main()
+{
+     printf("I'm the parent.\n");
+     printf("My PID is %d\n", getpid());
+     vector<string> str2g;
+     string text = "the big clock on the tower of the palace of westminster in london is";
+     int N = text.length();
+     str2g = trigrams(text);
+     for (auto& itr : str2g)
+     {
+	cout << itr << endl;
+     }
+     ofstream output_file("outline.txt");
+     ostream_iterator<string> output_iterator(output_file);
+     copy(str2g.begin(), str2g.end(), output_iterator);
+     output_file.close();
+     
+     int pid;
+     int status, died;
+     pid=fork();
+     switch(pid) 
+     {
+     	case -1:
+     	{
+     		printf("can't fork\n");
+     		exit(-1);
+     	}
+ 	case 0:
+ 	{
+ 		printf(" I'm the child of PID %d\n", getppid());
+ 		printf(" My PID is %d\n", getpid());
+ 		ifstream fin("outline.txt");
+    		string str;
+    		float procent=0;
+    		int full_str=0;
+    		getline(fin, str);
+    		map<string, float> words;
+    		for (int i = 0; i < str.size() - 2; i+=3)
+    		{
+        		words[str.substr(i, 3)]++;
+        		full_str++;
+    		}
+    		cout << "total trigrams :" << full_str << endl;
+    		cout << "unique trigrams :" << words.size() << endl;
+    		cout << "Result :" << endl;
+    		for (auto& iter : words)
+    		{
+        		cout <<"trigram: " << iter.first << "\tcount: (" << iter.second << ")\tfrequency: ("<< (100*iter.second)/ full_str <<")%\n";
+        		procent += ((100 * iter.second) / full_str);
+    		}
+    		cout << "\nTotal percent: " << procent<<endl;
+    
+ 		exit(0);
+ 	}
+ 	default:
+ 	{
+ 		sleep(10);
+ 		if (pid & 1){
+ 			kill(pid,SIGKILL);
+ 		}
+ 		died= wait(&status);
+ 		printf("kill process\n");
+ 	}
+    }
+    
+}
